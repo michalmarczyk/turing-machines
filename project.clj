f97c00b609e189eb708414e939d0cd7b43912ae6
Michał Marczyk <michal.marczyk@gmail.com>
2011-06-20 01:31:05 +0200

    Add swank-clojure to :dev-dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject turing-machines "0.1.0-SNAPSHOT"
   :description "A Turing machine emulator in Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]])
